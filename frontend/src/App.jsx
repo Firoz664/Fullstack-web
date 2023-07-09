64d87a6647423badc3600dfcfbc4a94183a0c687
Md Shams Firoz <Shams@antino.io>
2023-07-09 18:34:12 +0530

    change done app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,8 @@ function App() {
     <div>
       comming soon
     </div>
-      <h1>Hello-App</h1>
+      <h1>Hello-App </h1>
+      <h2>This comman platform sell and buy</h2>
        
     </>
   )
